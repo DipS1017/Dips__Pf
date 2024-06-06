5ab617637bd15c149bfcbc011b27748984c4e7a6
DipS1017 <150164648+DipS1017@users.noreply.github.com>
2024-06-06 16:10:50 +0545

    completed portfolio altogether will improve later

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,8 @@
 import './contact.css'
+import x from "../../assets/X_icon.svg"
+import github from "../../assets/github-142-svgrepo-com.svg"
+import gmail  from "../../assets/icons8-gmail.svg"
+import linkedin  from "../../assets/linkedin-svgrepo-com.svg"
 
 function Contact() {
   return (
@@ -16,10 +20,17 @@ function Contact() {
     <label >Message</label><br></br>
     <input type="text" id="subject" name="subject" placeholder="Message" ></input><br></br>
 
-    <button  className="Form-Submit" type="submit" value="Submit"/>
-
+    <button  className="Form-Submit" type="submit" value="Submit">Submit</button>
+<h1>OR</h1>
         </form>
-    
+    <article className="Contact__Img">
+    <a href="https://www.linkedin.com/in/dipesh-shrestha-145a1a300/" target="_blank"><img src={linkedin}/></a>
+    <a href="mailto:dipeshshrestha4220@gmail.com" target="_blank"><img src={gmail}/></a>
+    <a href="https://github.com/DipS1017" target="_blank"><img src={github}/></a>
+    <a href="https://x.com/dips__10" target="_blank"><img src={x}/></a>
+      
+    </article>
+    <article className="farewell">Thank You for Visiting! </article>
     
     
 
