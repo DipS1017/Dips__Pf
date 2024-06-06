3035e78d1bb7094f8c04c9a9bf891e0c132b62fc
DipS1017 <150164648+DipS1017@users.noreply.github.com>
2024-06-06 22:44:05 +0545

    removed loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,30 @@ import About from "./components/About/About";
 
 
 import Contact from "./components/Contact/Contact";
-import { useEffect, useState } from "react";
-import { ClipLoader } from "react-spinners";
+// import { useEffect, useState } from "react";
+// import { ClipLoader } from "react-spinners";
 import Project from "./components/Project/Project";
 
 
 function App() {
-  const [screenLoading, setScreenLoading] = useState(false);
-  useEffect(()=>{})
+  // const [screenLoading, setScreenLoading] = useState(false);
+  // useEffect(()=>{})
 
-  useEffect(() => {
-    setScreenLoading(true);
-    setTimeout(() => {
-      setScreenLoading(false);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   setScreenLoading(true);
+  //   setTimeout(() => {
+  //     setScreenLoading(false);
+  //   }, 1000);
+  // }, []);
 
   
  
 
   return (
-    <>
-      {screenLoading ? (
-        <ClipLoader color={"#66FDF0"} loading={screenLoading} className="RLoader" size={150} />
-      ) : (
+    // <>
+    //   {screenLoading ? (
+    //     <ClipLoader color={"#66FDF0"} loading={screenLoading} className="RLoader" size={150} />
+    //   ) : (
         <>
           <Navbar />
           <Home />
@@ -38,8 +38,8 @@ function App() {
           
           <Contact />
         </>
-      )}
-    </>
+      // )}
+    // </>
   );
 }
 
